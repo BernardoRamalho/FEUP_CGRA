50fedff908ccd2c2bb48377b441ce8bd7483f0e3
tiago-falves <tiago.falves98@gmail.com>
2019-05-28 19:16:46 +0100

    desenvolved eagle

--- projB/MyBird.js
+++ projB/MyBird.js
@@ -9,10 +9,19 @@ class MyBird extends CGFobject {
         super(scene);
 
         //MyBird Objects
-        this.head = new MyUnitCubeQuad(this.scene);
-        this.body = new MyUnitCubeQuad(this.scene);
-        this.rightWing = new MyTriangle(this.scene);
-        this.leftWing = new MyTriangle(this.scene);
+        this.topBeak = new MyPrism(this.scene, 3, 3, 0.3, 0.1);
+        this.head = new MyUnevenCylinder(this.scene,5,4,0.1,0.2,0.25);
+        this.head1 = new MyUnevenCylinder(this.scene,5,4,0.3,0.25,0.1);
+        this.body = new MyCylinder(this.scene,5,4,1.0,0.4);
+        this.neck = new MyUnevenCylinder(this.scene, 5, 4, 0.4, 0.4,0.2);
+        this.bottom = new MyUnevenCylinder(this.scene, 5, 4, 0.4, 0.05,0.4);
+        this.leftLeg = new MyCylinder(this.scene,5,5,0.5,0.1);
+        this.righttLeg = new MyCylinder(this.scene,5,5,0.5,0.1);
+        this.rightWingBeggining = new MyQuad(this.scene,0.8);
+        this.rightWingEnd = new MyTriangle(this.scene,0.8);
+        this.leftWingEnd = new MyTriangle(this.scene,0.8);
+        this.leftWingBeggining = new MyQuad(this.scene,0.8);
+
         //this.branch = new MyTreeBranch(this,2,0.5);
 
         //MyBird State Variables
@@ -26,6 +35,48 @@ class MyBird extends CGFobject {
         this.velocity = 0;
         this.position = [0, 3, 0];
 
+        this.initMaterials();
+
+    }
+
+    initMaterials() {
+
+        //Texture Wings
+        this.wingMaterial = new CGFappearance(this.scene);
+        this.wingMaterial.setAmbient(0.1, 0.1, 0.1, 1);
+        this.wingMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.wingMaterial.setSpecular(0.1, 0.1, 0.1, 1);
+        this.wingMaterial.setShininess(10.0);
+        this.wingMaterial.loadTexture("images/wings.jpg"); 
+        this.wingMaterial.setTextureWrap('REPEAT', 'REPEAT');
+
+        //Texture Body
+        this.bodyMaterial = new CGFappearance(this.scene);
+        this.bodyMaterial.setAmbient(0.1, 0.1, 0.1, 1);
+        this.bodyMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.bodyMaterial.setSpecular(0.1, 0.1, 0.1, 1);
+        this.bodyMaterial.setShininess(10.0);
+        this.bodyMaterial.loadTexture("images/eagleBody.jpg"); 
+        this.bodyMaterial.setTextureWrap('REPEAT', 'REPEAT');
+
+        //Texture Head
+        this.headMaterial = new CGFappearance(this.scene);
+        this.headMaterial.setAmbient(0.1, 0.1, 0.1, 1);
+        this.headMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.headMaterial.setSpecular(0.1, 0.1, 0.1, 1);
+        this.headMaterial.setShininess(10.0);
+        this.headMaterial.loadTexture("images/head.jpg"); 
+        this.headMaterial.setTextureWrap('REPEAT', 'REPEAT');
+
+        //Texture Beak
+        this.beakMaterial = new CGFappearance(this.scene);
+        this.beakMaterial.setAmbient(0.1, 0.1, 0.1, 1);
+        this.beakMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.beakMaterial.setSpecular(0.1, 0.1, 0.1, 1);
+        this.beakMaterial.setShininess(10.0);
+        this.beakMaterial.loadTexture("images/Beak.jpg"); 
+        this.beakMaterial.setTextureWrap('REPEAT', 'REPEAT');
+        
     }
 
     updatePosition(t, branches){
@@ -118,53 +169,124 @@ class MyBird extends CGFobject {
         //Changing Bird Position and Orientation
         
         this.scene.translate(...this.position);
+        //this.scene.scale(10,10,10);
 
         this.scene.rotate(-this.orientation, 0.0, 1.0, 0.0);
 
-        //Drawing the Body and Head
+        //Drawing Head
         this.scene.pushMatrix();
-
-        this.scene.translate(0.5, 1, 0);
-
+        this.scene.translate(0.5,0,0);
+        this.scene.rotate(90*Math.PI/180,0,0,1);
+        this.headMaterial.apply();
         this.head.display();
-
         this.scene.popMatrix();
 
+        this.scene.pushMatrix();
+        this.scene.translate(0.8,0,0);
+        this.scene.rotate(90*Math.PI/180,0,0,1);
+        this.headMaterial.apply();
+        this.head1.display();
+        this.scene.popMatrix();
+
+        //Drawing Neck
+        
+        this.scene.pushMatrix();
+        this.scene.translate(0.4,0,0);
+        this.scene.rotate(90*Math.PI/180,0,0,1);
+        this.headMaterial.apply();
+        this.neck.display();
+        this.scene.popMatrix();
+
+        //Drawing Top Beak
+
+        this.scene.pushMatrix();
+        this.scene.translate(0.8,0,0);
+        this.scene.rotate(-90*Math.PI/180,0,0,1);
+        //this.scene.rotate(180*Math.PI/180,0,0,1);
+        this.beakMaterial.apply();
+        this.topBeak.display();
+        this.scene.popMatrix();
+
+        //Drawing Body
+        
+        this.scene.pushMatrix();
+        this.scene.rotate(90*Math.PI/180,0,0,1);
+        this.bodyMaterial.apply();
+
         this.body.display();
+        this.scene.popMatrix();
+
+        //Drawing Bottom
+
+        this.scene.pushMatrix();
+        this.scene.translate(-1,0,0);
+        this.scene.rotate(90*Math.PI/180,0,0,1);
+        this.bodyMaterial.apply();
+        this.bottom.display();
+        this.scene.popMatrix();
+
+        //Drawing Left Leg
+
+        this.scene.pushMatrix();
+        this.scene.translate(-1,-0.2,0.2);
+        this.scene.rotate(90*Math.PI/180,0,0,1);
+        this.leftLeg.display();
+        this.scene.popMatrix();
+
+        //Drawing Right Leg
+
+        this.scene.pushMatrix();
+        this.scene.translate(-1,-0.2,-0.2);
+        this.scene.rotate(90*Math.PI/180,0,0,1);
+        this.leftLeg.display();
+        this.scene.popMatrix();
 
         //Drawing the Left Wing
         this.scene.pushMatrix();
-
         this.scene.translate(0, 0, 0.5);
-
         this.scene.scale(0.5, 0.5, 0.5);
-
-        this.scene.rotate(this.ang, 1.0, 0.0, 0.0);
-
-        this.scene.translate(0, 0, 1);
-
+        this.scene.rotate(this.ang+10*Math.PI/180, 1.0, 0.0, 0.0);
+        this.scene.translate(-0.8, 0, 2);
         this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.scene.rotate(Math.PI, 1, 1, 0);
+        this.wingMaterial.apply();
+        this.leftWingEnd.display();
+        this.scene.popMatrix();
 
-        this.leftWing.display();
-
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, 0.5);
+        this.scene.scale(0.5, 0.5, 0.5);
+        this.scene.translate(-0.8, 0, 0.4);
+        this.scene.rotate(this.ang, 1.0, 0.0, 0.0);
+        this.scene.rotate(-Math.PI/2, 1, 0, 0);
+        this.wingMaterial.apply();
+        this.leftWingBeggining.display();
         this.scene.popMatrix();
 
         //Drawing the Right Wing
-        this.scene.pushMatrix();
-
-        this.scene.translate(0, 0, -0.5);
-
-        this.scene.scale(0.5, 0.5, 0.5);
-
-        this.scene.rotate(-this.ang, 1.0, 0.0, 0.0);
-
-        this.scene.translate(0, 0, -1);
-
-        this.scene.rotate(-Math.PI/2, 1, 0, 0);
         
-        this.rightWing.display();
-
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, -0.5);
+        this.scene.scale(0.5, 0.5, 0.5);
+        this.scene.rotate(-this.ang-10*Math.PI/180, 1.0, 0.0, 0.0);
+        this.scene.translate(-0.8, 0, -2);
+        this.scene.rotate(-Math.PI/2, 1, 0, 0);
+        this.wingMaterial.apply();
+        this.rightWingEnd.display();
         this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, -0.5);
+        this.scene.scale(0.5, 0.5, 0.5);
+        this.scene.translate(-0.8, 0, -0.4);
+        this.scene.rotate(-this.ang, 1.0, 0.0, 0.0);
+        this.scene.rotate(-Math.PI/2, 1, 0, 0);
+        this.wingMaterial.apply();
+        this.rightWingBeggining.display();
+        this.scene.popMatrix();
+
+
+
 
         this.scene.popMatrix();
 
